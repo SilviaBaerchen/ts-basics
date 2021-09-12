f7cfdb467e7bbf609faabc9385eea2bc1b9d5c44
Silvia Bar <hi.silviabarcellini@gmail.com>
2021-09-12 21:25:58 +0200

    📝 complex types

--- basics.ts
+++ basics.ts
@@ -10,3 +10,24 @@ let isSubscribed: boolean;
 isSubscribed = true;
 
 
+// complex types
+
+let hobbies: string[]; //array of strings, but can be also of numbers, of booleans, ...
+hobbies = ["max", "max", "max"];
+
+let person: { //object declaration: I want this types in this exact order!
+    name: string,
+    age: number,
+};
+
+person = { //object example
+    name: "Max", 
+    age: 32,
+}
+
+let people : { // combine different types: array of objects declaration: I want an array of obects and the objects must have this structure!
+    name: string,
+    age: number,
+}[];
+
+
